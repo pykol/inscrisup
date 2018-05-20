4dd7a87bd2bd3b6780dccd900e46656dcfcf1d0e
Florian Hatat <inscrisup@hatat.me>
2018-05-20 16:07:30 +0200

    Ajout des modèles dans l'interface d'administration

--- parcoursup/admin.py
+++ parcoursup/admin.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Etudiant, Classe, Proposition, Action
+
+admin.site.register(Etudiant)
+admin.site.register(Classe)
+admin.site.register(Proposition)
+admin.site.register(Action)
