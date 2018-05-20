63087341ead206911055fa2c34145f2cc14ac104
Florian Hatat <inscrisup@hatat.me>
2018-05-20 16:30:04 +0200

    Vue basique pour une classe

--- parcoursup/urls.py
+++ parcoursup/urls.py
@@ -7,4 +7,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^classe/(?P<slug>[-\w]+)/$', views.classe, name='classe'),
 ]
--- parcoursup/views.py
+++ parcoursup/views.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Classe
 
 def index(request):
     return HttpResponse("Inscrisup")
+
+def classe(request, slug):
+    classe = get_object_or_404(Classe, slug=slug)
+    return HttpResponse("Détails en %s" % (classe.nom,))
