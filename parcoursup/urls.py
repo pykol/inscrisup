c8578b5528f75fb8b10824e6056c1fa3ad9ce105
Florian Hatat <inscrisup@hatat.me>
2019-05-23 08:02:37 +0200

    URL de l'interface synchrone Parcoursup

--- parcoursup/urls.py
+++ parcoursup/urls.py
@@ -21,25 +21,27 @@ from django.urls import path, include
 from . import views
 
 rest_parcoursup_urlpatterns = [
-    path('admissionCandidat', views.parcoursup.AdmissionView.as_view(), name='parcoursup_admission'),
+	path('admissionCandidat', views.parcoursup.AdmissionView.as_view(), name='parcoursup_admission'),
+	# Je pressens une blague de Parcoursup
+	path('admissionCandidat/admissionCandidat', views.parcoursup.AdmissionView.as_view(), name='parcoursup_admission'),
 ]
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('classe/<slug:slug>/', views.ClasseDetailView.as_view(), name='classe.details'),
-    path('classes/odf/', views.export_odf_classes, name='classes.odf'),
-    path('internat/', views.internat_detail, name='internat.details'),
-    path('etudiant/<int:pk>/', views.EtudiantDetailView.as_view(), name='etudiant.details'),
-    path('etudiant/<int:pk>/pdf_adresse', views.export_pdf_adresse_etudiant, name='etudiant.pdf_adresse'),
-    path('etudiant/<int:pk>/demission', views.etudiant_demission, name='etudiant.demission'),
-    path('proposition/ajout/', views.proposition_ajout, name='proposition.ajout'),
-    path('proposition/import/', views.parcoursup_import, name='proposition.parcoursup_import'),
-    path('proposition/import/auto', views.parcoursup_auto_import, name='proposition.parcoursup_auto_import'),
-    path('action/', views.ActionTodoListView.as_view(), name='action.liste'),
-    path('action/<int:pk>/', views.ActionDetailView.as_view(), name='action.details'),
-    path('action/<int:pk>/traiter', views.action_traiter, name='action.traiter'),
-    path('action/pdf_adresses/', views.export_pdf_adresses, name='action.export_pdf_adresses'),
-    path('action/pdf_adresses/definitif', views.export_pdf_adresses_definitif, name='action.export_pdf_adresses_definitif'),
+	path('', views.index, name='index'),
+	path('classe/<slug:slug>/', views.ClasseDetailView.as_view(), name='classe.details'),
+	path('classes/odf/', views.export_odf_classes, name='classes.odf'),
+	path('internat/', views.internat_detail, name='internat.details'),
+	path('etudiant/<int:pk>/', views.EtudiantDetailView.as_view(), name='etudiant.details'),
+	path('etudiant/<int:pk>/pdf_adresse', views.export_pdf_adresse_etudiant, name='etudiant.pdf_adresse'),
+	path('etudiant/<int:pk>/demission', views.etudiant_demission, name='etudiant.demission'),
+	path('proposition/ajout/', views.proposition_ajout, name='proposition.ajout'),
+	path('proposition/import/', views.parcoursup_import, name='proposition.parcoursup_import'),
+	path('proposition/import/auto', views.parcoursup_auto_import, name='proposition.parcoursup_auto_import'),
+	path('action/', views.ActionTodoListView.as_view(), name='action.liste'),
+	path('action/<int:pk>/', views.ActionDetailView.as_view(), name='action.details'),
+	path('action/<int:pk>/traiter', views.action_traiter, name='action.traiter'),
+	path('action/pdf_adresses/', views.export_pdf_adresses, name='action.export_pdf_adresses'),
+	path('action/pdf_adresses/definitif', views.export_pdf_adresses_definitif, name='action.export_pdf_adresses_definitif'),
 
-    path('parcoursup/', include(rest_parcoursup_urlpatterns)),
+	path('parcoursup/', include(rest_parcoursup_urlpatterns)),
 ]
